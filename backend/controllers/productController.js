5b5109513a628d6a7bb6de0217c1d2b70377eaae
Yogendra Rajput <yogendra.leetcode123@gmail.com>
2025-07-29 18:35:20 +0530

    add filter and get single product controllers and route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -158,12 +158,158 @@ exports.deleteProduct = async (req, res) => {
   }
 };
 
+//filterAndSortProduct
 
-exports.sortProducts=async(req,res)=>{
-  try{
+exports.filterAndSortProduct = async (req, res) => {
+  try {
+    const {
+      collection,
+      size,
+      color,
+      gender,
+      minPrice,
+      maxPrice,
+      sortBy,
+      search,
+      category,
+      material,
+      brand,
+      limit,
+    } = req.query;
 
-  }catch(error){
-      console.error(error);
-      return res.status(500).send("Server Error");
+    let query = {};
+
+    //filter logic
+    if (collection && collection.toLocaleLowerCase() != "all") {
+      query.collections = collection;
+    }
+    if (category && category.toLocaleLowerCase() != "all") {
+      query.category = category;
+    }
+
+    if (material) {
+      query.material = { $in: material.split(",") };
+    }
+    if (brand) {
+      query.brand = { $in: brand.split(",") };
+    }
+    if (size) {
+      query.sizes = { $in: size.split(",") };
+    }
+
+    //  if(color){
+    //    query.colors={$in:color.split(",")}
+    //  }
+    if (color) {
+      query.colors = { $in: [color] };
+    }
+
+    if (gender) {
+      query.gender = gender;
+    }
+
+    if (minPrice || maxPrice) {
+      query.price = {};
+      if (minPrice) {
+        query.price.$gte = Number(minPrice);
+      }
+      if (maxPrice) {
+        query.price.$lte = Number(maxPrice);
+      }
+    }
+
+    if (search) {
+      query.$or = [
+        { name: { $regex: search, $options: "i" } },
+        { description: { $regex: search, $options: "i" } },
+      ];
+    }
+
+    // Sort Logic
+    let sort = {};
+    if (sortBy) {
+      switch (sortBy) {
+        case "priceAsc":
+          sort = { price: 1 };
+          break;
+        case "priceDesc":
+          sort = { price: -1 };
+          break;
+        case "popularity":
+          sort = {
+            rating: -1,
+          };
+          break;
+        default:
+          break;
+      }
+    }
+
+    console.log("Query Parameters:", query);
+    // find and apply sorting and limit
+    let products = await Product.find(query)
+      .sort(sort)
+      .limit(Number(limit) || 0);
+
+    res.status(200).json({
+      success: true,
+      message: "Products filered Successfully",
+      products,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server Error");
   }
-}
+};
+
+// getSingleProductDetails
+exports.getSingleProductDetails = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const productDetails = await Product.findById(productId);
+    if (!productDetails) {
+      return res.status(404).json({
+        success: false,
+        message: "Product Details not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Product Details found Successfully",
+      productDetails,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+};
+
+//getSimilarProducts
+exports.getSimilarProducts = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    const similarProducts = await Product.find({
+      _id: { $ne: id }, //Exclude the current product ID
+      gender: product.gender,
+      category: product.category,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Similar Products Found Successfully",
+      similarProducts,
+    }).limit(4);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,9 @@ const {
   createProduct,
   updateProduct,
   deleteProduct,
+  filterAndSortProduct,
+  getSingleProductDetails,
+  getSimilarProducts,
 } = require("../controllers/productController");
 
 
@@ -19,14 +22,29 @@ router.post("/",protect,isAdmin,createProduct);
 //@route PUT /api/products/:id
 //@ Update the existing product by its ID
 //@ Access Private/Admin
-
 router.put("/:id",protect,isAdmin,updateProduct);
 
 //@route DELETE /api/products/:id
 //@ Delete the existing product by its ID
 //@ Access Private/Admin
-
 router.delete("/:id",protect,isAdmin,deleteProduct);
+
+
+//@route Get /api/products
+//@ Get all products with optional query filter
+//@ Access Public
+router.get("/", filterAndSortProduct);
+
+//@route Get /api/products/:id
+//@ Get a single Product Details by its ID
+//@ Access Public
+router.get("/:id",getSingleProductDetails)
+
+//@route Get /api/products/similar/:id
+//@ Retrieve Similar Product based on the current product's gender and category
+//@ Access Public
+
+router.get("/similar/:id",getSimilarProducts)
 
 
 module.exports=router;
